52e1e09c441e7df9f801b855e07b4ad3da228ce3
shriramk0706 <73196443+shriramk0706@users.noreply.github.com>
2021-05-18 23:55:59 +0530

    Update SampleProgram.java

--- SampleProgram.java
+++ SampleProgram.java
@@ -2,7 +2,7 @@ public class SampleProgram {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Jenkins");
-		List<Integer> list = new ArrayList<>(Arrays.asList{20, 30, 10, 0});
-		System.out.println("print list of array:"+list
+		List<Integer> list = new ArrayList<>(Arrays.asList(20, 30, 10, 90));
+		System.out.println("print list of array:"+list);
 	}
 }
